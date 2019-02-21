8303ef0268367b68f590ef74e20be361ca13b6a6
JoseLuisMejiaG <joseluismejia.2197@gmail.com>
2019-02-21 08:41:59 -0600

    Second Activity
    Outlet and Action activity

---
+++ JoseLuis.Mejia210219OA/JoseLuis.Mejia210219OATests/JoseLuis_Mejia210219OATests.swift
@@ -0,0 +1,34 @@
+//
+//  JoseLuis_Mejia210219OATests.swift
+//  JoseLuis.Mejia210219OATests
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 2/21/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import XCTest
+@testable import JoseLuis_Mejia210219OA
+
+class JoseLuis_Mejia210219OATests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
